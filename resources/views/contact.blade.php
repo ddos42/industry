e71b55f6861e038528d6cc7add49790bd26ca583
alexmayo <am@123.com>
2017-05-05 10:05:02 +1200

    update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -28,16 +28,16 @@ We are open from 8:30am to 5:30pm, Monday to Friday, NZST. </p> </div>
 </form>
 </div>
 <div class="col-md-6 contact_map">
-<div class="google_maps">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5041.755931644769!2d174.7760139530989!3d-41.29252568800015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38afd770c8aad7%3A0xe35be44c3bf6959c!2sCuba+Mall%2C+Te+Aro%2C+Wellington+6011!5e0!3m2!1sen!2snz!4v1493932685226" frameborder="0" style="border:0" allowfullscreen></iframe>
-</div>
-                    <!-- end of google-maps -->
-<!--  <div id="map"></div>-->
+ <iframe
+  width="600"
+  height="450"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAtUaNadLiXMIjxFe9n3xb4368SGm9B2iQ
+    &q=Space+Needle,Seattle+WA" allowfullscreen>
+</iframe>
 
 
-
 </div>
 </div>
 </div>
-
 @stop
