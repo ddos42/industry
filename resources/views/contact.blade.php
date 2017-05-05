84af517e5cd5a1e0ae825d5a4cdf28f5ffac4058
alexmayo <am@123.com>
2017-05-05 22:38:52 +1200

    updatdde

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -28,8 +28,7 @@ We are open from 8:30am to 5:30pm, Monday to Friday, NZST. </p> </div>
 </form>
 </div>
 <div class="col-md-6 contact_map">
-    <iframe id="google_map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2997.7669190414927!2d174.77413481580857!3d-41.29217694869175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38afd770c8aad7%3A0xe35be44c3bf6959c!2sCuba+Mall%2C+Te+Aro%2C+Wellington+6011!5e0!3m2!1sen!2snz!4v1493939614797"  height="450" frameborder="0" style="border:0"></iframe>
-</iframe>
+    <iframe id="google_map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2997.7669190414927!2d174.77413481580857!3d-41.29217694869175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38afd770c8aad7%3A0xe35be44c3bf6959c!2sCuba+Mall%2C+Te+Aro%2C+Wellington+6011!5e0!3m2!1sen!2snz!4v1493939614797"  height="450" ></iframe>
 
 
 </div>
